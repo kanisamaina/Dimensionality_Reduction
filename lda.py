2764d0c901a714c1dd96c312e35bf8ed81092944
Kanishka <55542796+kanisamaina@users.noreply.github.com>
2020-07-28 21:31:28 +0530

    Add files via upload

---
+++ lda.py
@@ -0,0 +1,94 @@
+#Linear Discriminant Analysis
+
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing the dataset
+dataset=pd.read_csv('Wine.csv')
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,-1].values
+
+#Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.25,random_state=0)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+
+#Applying LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA(n_components = 2)
+X_train=lda.fit_transform(X_train,Y_train)
+X_test=lda.transform(X_test)
+
+#Training the Logistic Regression model on the Training Set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0,solver='lbfgs')
+classifier.fit(X_train,Y_train)
+
+
+#Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+Y_pred = classifier.predict(X_test)
+cm=confusion_matrix(Y_test, Y_pred)
+print(cm)
+accuracy_score(Y_test, Y_pred)
+
+# Visualising the Training set results
+#ListedColormap class help us to colorize the data points.
+from matplotlib.colors import ListedColormap
+#Create local variables X_set and y_set. Because we use these #variables again in the test set
+X_set, Y_set = X_train, Y_train
+#Create the grid. step=0.01 means all the pixels were actually with #a 0.01 resolution. min and max of the 
+#X_Set use with minus ana plus one to prevent ponits to be squeezed #on the axes.
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+#This is the line applying the classifier on all the pixel #observation points. It colors all the red pixel 
+#points and the blue pixel points. contour function make the contour #between red and blue regions.
+plt.contourf(X1,X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),alpha=0.75, 
+             cmap = ListedColormap(('red','blue','green')))
+#plot the limits of the age and the estimated salary lines.
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+#This loop here plots all the data points that are the real values.
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1], c = ListedColormap(('red', 'blue','green'))(i), label = j)
+#Add the name of the plot and the labels.
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+# Visualising the Testing set results
+#ListedColormap class help us to colorize the data points.
+from matplotlib.colors import ListedColormap
+#Create local variables X_set and y_set. Because we use these #variables again in the test set
+X_set, Y_set = X_test, Y_test
+#Create the grid. step=0.01 means all the pixels were actually with #a 0.01 resolution. min and max of the 
+#X_Set use with minus ana plus one to prevent ponits to be squeezed #on the axes.
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+#This is the line applying the classifier on all the pixel #observation points. It colors all the red pixel 
+#points and the blue pixel points. contour function make the contour #between red and blue regions.
+plt.contourf(X1,X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),alpha=0.75, 
+             cmap = ListedColormap(('red','blue','green')))
+#plot the limits of the age and the estimated salary lines.
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+#This loop here plots all the data points that are the real values.
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1], c = ListedColormap(('red', 'blue','green'))(i), label = j)
+#Add the name of the plot and the labels.
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
